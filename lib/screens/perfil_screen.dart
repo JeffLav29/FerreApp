4d02194208da35a708d4d5ba61ddf8d1473b479e
Jefferson <jlavadoastuhuaman@gmail.com>
2025-06-16 02:17:32 -0500

    Agregando screen de perfil

---
+++ lib/screens/perfil_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PerfilScreen extends StatefulWidget{
+  const PerfilScreen({super.key});
+  
+  @override
+  State<PerfilScreen> createState() => _PerfilScreen();
+}
+
+class _PerfilScreen extends State<PerfilScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
